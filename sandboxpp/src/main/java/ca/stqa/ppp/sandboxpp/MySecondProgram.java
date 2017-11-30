9968aeb7881200ad522186c009b8a3baeb431a58
Angy1 <angelica.nm@hotmail.com>
2017-11-29 21:57:22 -0500

    MySecondProgram

--- sandboxpp/src/main/java/ca/stqa/ppp/sandboxpp/MySecondProgram.java
+++ sandboxpp/src/main/java/ca/stqa/ppp/sandboxpp/MySecondProgram.java
@@ -1,4 +1,6 @@
-public class MySecondProgram{
+package ca.stqa.ppp.sandboxpp;
+
+public class MySecondProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
